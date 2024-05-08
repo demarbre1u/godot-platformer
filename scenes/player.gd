d15b44837d18b99855f42243bc3a686f1e2dcfa0
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2024-05-08 21:11:01 +0200

    Slightly reorganizing the main menu, added custom control for the player

--- scenes/player.gd
+++ scenes/player.gd
@@ -21,12 +21,12 @@ func _physics_process(delta):
 		sprite_2d.animation = "jumping"
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		sprite_2d.set_flip_h(direction < 0)
