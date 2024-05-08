6a57c1195615c9a2562ac995881b35398b54a980
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2024-05-08 22:08:05 +0200

    Added a simple parallax effect to the main menu

---
+++ scenes/ParallaxBackground.gd
@@ -0,0 +1,14 @@
+extends ParallaxBackground
+
+@export var parallaxSpeedX: float = 50.0
+@export var parallaxSpeedY: float = 30.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	scroll_offset.x -= delta * parallaxSpeedX
+	scroll_offset.y -= delta * parallaxSpeedY
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 func _on_level_1_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
