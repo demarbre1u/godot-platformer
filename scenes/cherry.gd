e3a42917151e7c5acb1e5f3dc4430d675882b924
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2024-05-08 21:05:18 +0200

    Added a collected animations to collectables

--- scenes/cherry.gd
+++ scenes/cherry.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		animated_sprite_2d.animation = "collected"
 		game_manager.add_to_score(10)
+
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if(animated_sprite_2d.animation == "collected"):
 		queue_free()
