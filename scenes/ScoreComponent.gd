f3c18ce35699493732d14212cbf9cd109876376c
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2024-05-27 20:52:56 +0200

    More file reorganizing, added more collectables to the game

---
+++ scenes/ScoreComponent.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name ScoreComponent
+
+@export var score = 5
--- scripts/collectable.gd
+++ scripts/collectable.gd
@@ -2,13 +2,12 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var score_component = $ScoreComponent
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		animated_sprite_2d.animation = "collected"
-		game_manager.add_to_score(10)
-
-
+		animated_sprite_2d.play("collected")
+		game_manager.add_to_score(score_component.score)
 
 func _on_animated_sprite_2d_animation_finished():
 	if(animated_sprite_2d.animation == "collected"):
