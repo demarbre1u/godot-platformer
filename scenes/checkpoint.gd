3bb8843846af768da3fad6b6e0c055fcb75d4039
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2024-05-27 19:57:56 +0200

    Fixed errors in the debugger console

--- scenes/checkpoint.gd
+++ scenes/checkpoint.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene_to_packed(target_scene)
+		get_tree().call_deferred("change_scene_to_packed", target_scene);
