94083fb07bc931d4fc408985cd49b45a4497fe8d
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2024-05-27 20:01:20 +0200

    Reorganized the files in the project

--- scripts/ui/PausePanel.gd
+++ scripts/ui/PausePanel.gd
@@ -3,7 +3,7 @@ extends Panel
 @onready var game_manager = %GameManager
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if game_manager.is_paused: 
 		show()
 	else: 
